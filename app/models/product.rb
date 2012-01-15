fde7ee5821ea9c415513b7d575dd6faf8d6fc3be
Martin Moizard <martin.moizard@gmail.com>
2012-01-15 19:21:39 +0100

    new controller store

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates_length_of :title, minimum: 10
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
+
+  root :to => 'store#index', as: 'store'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
