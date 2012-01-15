2243007f04e978a7d4c25e05898721cc07974571
Martin Moizard <martin.moizard@gmail.com>
2012-01-15 13:07:07 +0100

    Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -4,4 +4,29 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any? == true
+    assert product.errors[:description].any? == true
+    assert product.errors[:price].any? == true
+    assert product.errors[:image_url].any? == true
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(title: "My book title",
+      description: "yyy",
+      image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid? == true
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+    
+    product.price = 0
+    assert product.invalid? == true
+    assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
+  end
 end
